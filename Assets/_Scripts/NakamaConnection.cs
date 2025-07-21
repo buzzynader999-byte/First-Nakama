c22f4ca7b31732e435de062eb8b516c2f789f57c
buzzynader999-byte <buzzynader999@gmail.com>
2025-07-21 12:40:18 +0330

    Nakama Sdk added + we made a simple connection script

--- Assets/_Scripts/NakamaConnection.cs
+++ Assets/_Scripts/NakamaConnection.cs
@@ -1,16 +1,35 @@
+using System;
+using Nakama;
 using UnityEngine;
 
-public class NakamaConnection : MonoBehaviour
+namespace _Scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class NakamaConnection : MonoBehaviour
     {
-        
-    }
+        private readonly string _scheme = "http";
+        private readonly string _host = "127.0.0.1";
+        private readonly int _port = 7350;
+        private readonly string _key = "defaultkey";
+        IClient _client;
+        ISocket _socket;
+        ISession _session;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        async void Start()
+        {
+            try
+            {
+                _client = new Client(_scheme, _host, _port, _key, UnityWebRequestAdapter.Instance);
+                _session = await _client.AuthenticateDeviceAsync(SystemInfo.deviceUniqueIdentifier);
+                _socket = _client.NewSocket();
+                await _socket.ConnectAsync(_session, true);
+                print(_session);
+                print(_socket);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
     }
 }
