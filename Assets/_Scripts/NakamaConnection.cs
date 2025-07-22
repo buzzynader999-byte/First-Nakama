0ea1d85e1780eb967e7f8eb337b1113d41708776
buzzynader999-byte <buzzynader999@gmail.com>
2025-07-22 09:02:56 +0330

    feat : A simple matchmaker added to nakama connection script
    - a button added in UI for find match

--- Assets/_Scripts/NakamaConnection.cs
+++ Assets/_Scripts/NakamaConnection.cs
@@ -14,21 +14,51 @@ namespace _Scripts
         ISocket _socket;
         ISession _session;
 
+        private string _ticket;
 
         async void Start()
         {
-            try
-            {
+            // try
+            // {
                 _client = new Client(_scheme, _host, _port, _key, UnityWebRequestAdapter.Instance);
                 _session = await _client.AuthenticateDeviceAsync(SystemInfo.deviceUniqueIdentifier);
                 _socket = _client.NewSocket();
                 await _socket.ConnectAsync(_session, true);
+
+                _socket.ReceivedMatchmakerMatched += OnReceivedMatchMakerMatched;
+
                 print(_session);
                 print(_socket);
-            }
-            catch (Exception e)
+            // }
+            // catch (Exception e)
+            // {
+            //     Debug.Log(e.Message);
+            // }
+        }
+
+        public async void FindMatch()
+        {
+            print("FindMatch");
+            // try
+            // {
+                var matchmakingTicker = await _socket.AddMatchmakerAsync("*", 2, 2, null, null);
+                _ticket = matchmakingTicker.Ticket;
+            // }
+            // catch (Exception e)
+            // {
+            //     Debug.Log(e.Message);
+            // }
+        }
+
+        async void OnReceivedMatchMakerMatched(IMatchmakerMatched matchmaker)
+        {
+            print("Found matchmaker");
+            var match = await _socket.JoinMatchAsync(matchmaker);
+            print("joined matchmaker");
+            print(match.Self.SessionId);
+            foreach (var users in match.Presences)
             {
-                Debug.Log(e.Message);
+                Debug.Log(users.SessionId);
             }
         }
     }
