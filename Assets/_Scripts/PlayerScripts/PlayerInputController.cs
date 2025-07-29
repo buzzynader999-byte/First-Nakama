3a02af255a8525b1b577ca64c29f968fb3838a27
nader <buzzynader999@gmail.com>
2025-07-29 09:03:29 +0330

    Fish animations for jump, run and idle is fine

--- Assets/_Scripts/PlayerScripts/PlayerInputController.cs
+++ Assets/_Scripts/PlayerScripts/PlayerInputController.cs
@@ -6,30 +6,36 @@ namespace _Scripts.Entities
 {
     public class PlayerInputController : MonoBehaviour
     {
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Jump = Animator.StringToHash("Jump");
+        [SerializeField] private Animator bodyAnimator;
         private bool _isInputChanged;
         public bool IsInputChanged => _isInputChanged;
         [SerializeField] PlayerMovementController playerMovementController;
         public PlayerInputControllerDetails InputDetails { set; get; } = new PlayerInputControllerDetails();
+        PlayerInputControllerDetails _lastInputDetails = new PlayerInputControllerDetails();
 
         private void Update()
         {
-            var horizontal = GatherInput.Controll.Player.Move.ReadValue<Vector2>().x;
-            //print(horizontal);
-            //var jump = GatherInput.Controll.Player.Jump.ReadValue<Vector2>();
+            _lastInputDetails.HorizontalInput = GatherInput.Controll.Player.Move.ReadValue<Vector2>().x;
+            _lastInputDetails.Jump = GatherInput.Controll.Player.Jump.IsPressed();
 
-            _isInputChanged = CheckForInptChange(horizontal);
-            if (_isInputChanged)
-            {
-                print("input changed");
-            }
-            InputDetails.HorizontalInput = horizontal;
-            
+            _isInputChanged = CheckForInptChange(_lastInputDetails);
+            if (!_isInputChanged)
+                return;
+
+            bodyAnimator.SetFloat(Horizontal, Mathf.Abs(_lastInputDetails.HorizontalInput));
+            InputDetails.HorizontalInput = _lastInputDetails.HorizontalInput;
+            InputDetails.Jump = _lastInputDetails.Jump;
             playerMovementController.SetHorizontal(InputDetails.HorizontalInput);
+            if (_lastInputDetails.Jump)
+                playerMovementController.Jump();
         }
 
-        private bool CheckForInptChange(float horizontal)
+        private bool CheckForInptChange(PlayerInputControllerDetails details)
         {
-            return horizontal != InputDetails.HorizontalInput;
+            return !Mathf.Approximately(details.HorizontalInput, InputDetails.HorizontalInput) ||
+                   details.Jump != InputDetails.Jump;
         }
     }
 
